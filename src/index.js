1e4be6e977d0dced905a2643d048fd9efceeb015
yanyan155 <jm@tut.by>
2018-02-20 11:10:36 +0300

    100% tests

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for(var i = 0; i<preferences.length; i++) {
+  	var firstIndex = preferences[i];
+  	var secondIndex = preferences[firstIndex-1];
+  	var thirdIndex = preferences[secondIndex-1];
+  	if (thirdIndex -1 === i) {
+  		count ++;
+  	}
+  }
+  return Math.floor(count/3);
 };
